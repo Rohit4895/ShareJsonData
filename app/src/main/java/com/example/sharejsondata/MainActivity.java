90c54735aaecae0a3f293a0bcf9c05d84b79fe18
rohit <rohit.ramtirth@thinkitive.com>
2019-08-08 12:35:04 +0530

    permission check added

--- app/src/main/java/com/example/sharejsondata/MainActivity.java
+++ app/src/main/java/com/example/sharejsondata/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.sharejsondata;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button send, receive;
+    private final int MY_PERMISSION_REQUEST=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         send.setOnClickListener(this);
         receive.setOnClickListener(this);
+
+        if ((ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+                && (ContextCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)){
+
+           askPermission();
+        }
+    }
+
+    private void askPermission(){
+        ActivityCompat.requestPermissions(this,new String[]{ACCESS_FINE_LOCATION,
+                ACCESS_COARSE_LOCATION},MY_PERMISSION_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case MY_PERMISSION_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+
+                }else{
+                    showMessage("It is mandatory to accept all permissions...");
+                }
+                break;
+        }
+    }
+
+    private void showMessage(String message){
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        askPermission();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .create();
+
+        alertDialog.show();
     }
 
     @Override
